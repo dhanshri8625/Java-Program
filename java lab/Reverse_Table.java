0a9516bd5dcaf384428081e3adca23d9cdfbfc6d
dhanshri8625 <dhanshrighadge2004@gmail.com>
2024-08-14 15:15:11 +0530

    Add files via upload

---
+++ java lab/Reverse_Table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Reverse_Table {
+    public static void main(String[]args){
+        int Num = 0, i = 1;
+        Scanner S = new Scanner(System.in);
+  
+        System.out.println("Enter a  Number:");
+        Num = S.nextInt();
+         
+        //while(i <= 10){
+            for(i=10;i>=1;i--){
+            System.out.println(Num + " * " + i + " = " + Num*i);
+            //i++;
+        }
+    }
+}
