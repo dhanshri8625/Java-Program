c4e94ded8d6279a876e3f79b955561a3618da600
dhanshri8625 <dhanshrighadge2004@gmail.com>
2024-08-21 15:28:52 +0530

    Add files via upload

---
+++ java lab/Single_Print.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Single_Print {
+    public static void main(String[]args)
+    {
+        Scanner S = new Scanner(System.in);
+        System.out.print("Enter Student Name:\t");
+        String Name = S.nextLine();
+        
+        System.out.print("Enter Roll No:\t");
+        int Num = S.nextInt();
+
+        System.out.print("Enter Course:\t");
+        String Course = System.console().readLine();
+
+        System.out.print("Enter City:\t");
+        String City = System.console().readLine();
+
+
+        System.out.println(" Student Name: " +Name+ " \n Roll No: " +Num+ " \n Enetr Course: "+Course+ " \n Enter City:d "+City);
+
+    
+    }    
+    }
+    
+
